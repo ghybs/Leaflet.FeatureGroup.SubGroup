16e95452044a4a9c8ca5fcab6f7e831cb38bc7e3
ghybs <ghybs1@gmail.com>
2016-03-12 23:20:34 +0400

    Corrected code indentation
    from tabs to 4 whitespaces.

--- leaflet.featuregroup.subgroup-src.js
+++ leaflet.featuregroup.subgroup-src.js
@@ -7,149 +7,149 @@
 
 
 (function (root, factory) {
-	if (typeof define === 'function' && define.amd) {
-		define(['leaflet'], function (L) {
-			return (root.L.FeatureGroup.SubGroup = factory(L));
-		});
-	} else if (typeof module === 'object' && module.exports) {
-		module.exports = factory(require('leaflet'));
-	} else {
-		root.L.FeatureGroup.SubGroup = factory(root.L);
-	}
+    if (typeof define === 'function' && define.amd) {
+        define(['leaflet'], function (L) {
+            return (root.L.FeatureGroup.SubGroup = factory(L));
+        });
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory(require('leaflet'));
+    } else {
+        root.L.FeatureGroup.SubGroup = factory(root.L);
+    }
 }(this, function (L) {
 
-	var FG = L.FeatureGroup,
-	    FGproto = FG.prototype,
-	    LG = L.LayerGroup;
+    var FG = L.FeatureGroup,
+        FGproto = FG.prototype,
+        LG = L.LayerGroup;
 
 
-	var SubGroup = FG.extend({
+    var SubGroup = FG.extend({
 
-		statics: {
-			version: '0.1.0'
-		},
+        statics: {
+            version: '0.1.0'
+        },
 
-		/**
-		 * Instantiates a SubGroup.
-		 * @param parentGroup (L.LayerGroup) (optional)
-		 * @param layersArray (L.Layer[]) (optional)
-		 */
-		initialize: function (parentGroup, layersArray) {
-			FGproto.initialize.call(this, layersArray);
+        /**
+         * Instantiates a SubGroup.
+         * @param parentGroup (L.LayerGroup) (optional)
+         * @param layersArray (L.Layer[]) (optional)
+         */
+        initialize: function (parentGroup, layersArray) {
+            FGproto.initialize.call(this, layersArray);
 
-			this.setParentGroup(parentGroup);
-		},
+            this.setParentGroup(parentGroup);
+        },
 
-		/**
-		 * Changes the parent group into which child markers are added to /
-		 * removed from.
-		 * @param parentGroup (L.LayerGroup)
-		 * @returns {SubGroup} this
-		 */
-		setParentGroup: function (parentGroup) {
-			this._parentGroup = parentGroup;
+        /**
+         * Changes the parent group into which child markers are added to /
+         * removed from.
+         * @param parentGroup (L.LayerGroup)
+         * @returns {SubGroup} this
+         */
+        setParentGroup: function (parentGroup) {
+            this._parentGroup = parentGroup;
 
-			// onAdd
-			this.onAdd =
-				parentGroup instanceof LG ?
-					(
-						typeof parentGroup.addLayers === "function" ?
-							this._onAddToGroupBatch :
-							this._onAddToGroup
-					) :
-					this._onAddToMap;
+            // onAdd
+            this.onAdd =
+                parentGroup instanceof LG ?
+                    (
+                        typeof parentGroup.addLayers === "function" ?
+                            this._onAddToGroupBatch :
+                            this._onAddToGroup
+                    ) :
+                    this._onAddToMap;
 
-			// onRemove
-			this.onRemove =
-				parentGroup instanceof LG ?
-					(
-						typeof parentGroup.removeLayers === "function" ?
-							this._onRemoveFromGroupBatch :
-							this._onRemoveFromGroup
-					) :
-					this._onRemoveFromMap;
+            // onRemove
+            this.onRemove =
+                parentGroup instanceof LG ?
+                    (
+                        typeof parentGroup.removeLayers === "function" ?
+                            this._onRemoveFromGroupBatch :
+                            this._onRemoveFromGroup
+                    ) :
+                    this._onRemoveFromMap;
 
-			return this;
-		},
+            return this;
+        },
 
-		/**
-		 * Removes the current sub-group from map before changing the parent
-		 * group. Re-adds the sub-group to map if it was before changing.
-		 * @param parentGroup (L.LayerGroup)
-		 * @returns {SubGroup} this
-		 */
-		setParentGroupSafe: function (parentGroup) {
-			var map = this._map;
+        /**
+         * Removes the current sub-group from map before changing the parent
+         * group. Re-adds the sub-group to map if it was before changing.
+         * @param parentGroup (L.LayerGroup)
+         * @returns {SubGroup} this
+         */
+        setParentGroupSafe: function (parentGroup) {
+            var map = this._map;
 
-			if (map) {
-				map.removeLayer(this);
-			}
+            if (map) {
+                map.removeLayer(this);
+            }
 
-			this.setParentGroup(parentGroup);
+            this.setParentGroup(parentGroup);
 
-			if (map) {
-				map.addLayer(this);
-			}
+            if (map) {
+                map.addLayer(this);
+            }
 
-			return this;
-		},
+            return this;
+        },
 
-		/**
-		 * Returns the current parent group.
-		 * @returns {*}
-		 */
-		getParentGroup: function () {
-			return this._parentGroup;
-		},
-
-
-		// For parent groups with batch methods (addLayers and removeLayers)
-		// like MarkerCluster.
-		_onAddToGroupBatch: function (map) {
-			var layersArray = this.getLayers();
-
-			this._map = map;
-			this._parentGroup.addLayers(layersArray);
-		},
-
-		_onRemoveFromGroupBatch: function (map) {
-			var layersArray = this.getLayers();
-
-			this._parentGroup.removeLayers(layersArray);
-			this._map = null;
-		},
+        /**
+         * Returns the current parent group.
+         * @returns {*}
+         */
+        getParentGroup: function () {
+            return this._parentGroup;
+        },
 
 
-		// For other parent layer groups.
-		_onAddToGroup: function (map) {
-			var parentGroup = this._parentGroup;
+        // For parent groups with batch methods (addLayers and removeLayers)
+        // like MarkerCluster.
+        _onAddToGroupBatch: function (map) {
+            var layersArray = this.getLayers();
 
-			this._map = map;
-			this.eachLayer(parentGroup.addLayer, parentGroup);
-		},
+            this._map = map;
+            this._parentGroup.addLayers(layersArray);
+        },
 
-		_onRemoveFromGroup: function (map) {
-			var parentGroup = this._parentGroup;
+        _onRemoveFromGroupBatch: function (map) {
+            var layersArray = this.getLayers();
 
-			this.eachLayer(parentGroup.removeLayer, parentGroup);
-			this._map = null;
-		},
-
-
-		// Defaults to standard FeatureGroup behaviour when parent group is not
-		// specified or is not a type of LayerGroup.
-		_onAddToMap: FGproto.onAdd,
-		_onRemoveFromMap: FGproto.onRemove
-
-	});
+            this._parentGroup.removeLayers(layersArray);
+            this._map = null;
+        },
 
 
+        // For other parent layer groups.
+        _onAddToGroup: function (map) {
+            var parentGroup = this._parentGroup;
 
-	// Supply with a factory for consistency with Leaflet.
-	L.featureGroup.subGroup = function (parentGroup, options) {
-		return new FG.SubGroup(parentGroup, options);
-	};
+            this._map = map;
+            this.eachLayer(parentGroup.addLayer, parentGroup);
+        },
 
-	// Just return a value to define the module export.
-	return SubGroup;
+        _onRemoveFromGroup: function (map) {
+            var parentGroup = this._parentGroup;
+
+            this.eachLayer(parentGroup.removeLayer, parentGroup);
+            this._map = null;
+        },
+
+
+        // Defaults to standard FeatureGroup behaviour when parent group is not
+        // specified or is not a type of LayerGroup.
+        _onAddToMap: FGproto.onAdd,
+        _onRemoveFromMap: FGproto.onRemove
+
+    });
+
+
+
+    // Supply with a factory for consistency with Leaflet.
+    L.featureGroup.subGroup = function (parentGroup, options) {
+        return new FG.SubGroup(parentGroup, options);
+    };
+
+    // Just return a value to define the module export.
+    return SubGroup;
 }));
